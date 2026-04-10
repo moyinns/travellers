4d0aa3373754e5b4dbdcb926fee8c1bde2a21e04
Moyinoluwa Akinnawonu <moyinaki@gmail.com>
2026-04-10 01:25:42 +0100

    added the ninja attack system
    need to fix the infinite loop it runs

--- fight scene/fight_screen.gd
+++ fight scene/fight_screen.gd
@@ -28,10 +28,80 @@ extends Node2D
 @onready var dash_punch_button: Button = $"dash punch button"
 @onready var lgh_kick_button: Button = $"lgh kick button"
 
+var moves_made = 0
+var enemy_health = 40
+var enemy_skill = 100
+var ninja_moves = [["flying swallow", 15, 4],
+					["izunna drop", 10, 2],
+					["wind path", 7, 0],
+					["jumping double slash", 10, 2],
+					["art of the inferno", 25, 15],
+					["windmill slash", 30, 20],
+					["helmet splitter", 40, 30],
+					["shinobi aruki", 0, 30],
+					["double breathing", 0, 45],
+					["distraction", 0, 20]]
 
-var enemy_health = 10
-
-
+func ninja_attack():
+	var attack_made = false
+	var attack_damage = 0
+	var skill_damage = 0
+	var attack_name = ""
+	while not attack_made:
+		var num = randi_range(1, 100)
+		var random_row = randi_range(0,2)
+		if enemy_health >= 30:
+			if num <= 5:
+				attack_damage = ninja_moves[random_row+3][1]
+				skill_damage = ninja_moves[random_row+3][2]
+				attack_name = ninja_moves[random_row+3][0]
+			elif num <= 30:
+				attack_damage = ninja_moves[random_row+6][1]
+				skill_damage = ninja_moves[random_row+6][2]
+				attack_name = ninja_moves[random_row+6][0]
+			else:
+				attack_damage = ninja_moves[random_row][1]
+				skill_damage = ninja_moves[random_row][2]
+				attack_name = ninja_moves[random_row][0]
+		elif enemy_health >= 20:
+			if num <= 33:
+				attack_damage = ninja_moves[random_row+3][1]
+				skill_damage = ninja_moves[random_row+3][2]
+				attack_name = ninja_moves[random_row+3][0]
+			elif num <= 66:
+				attack_damage = ninja_moves[random_row+6][1]
+				skill_damage = ninja_moves[random_row+6][2]
+				attack_name = ninja_moves[random_row+6][0]
+			else:
+				attack_damage = ninja_moves[random_row][1]
+				skill_damage = ninja_moves[random_row][2]
+				attack_name = ninja_moves[random_row][0]
+		else:
+			if num <= 5:
+				attack_damage = ninja_moves[random_row+6][1]
+				skill_damage = ninja_moves[random_row+6][2]
+				attack_name = ninja_moves[random_row+6][0]
+			elif num <= 20:
+				attack_damage = ninja_moves[random_row+3][1]
+				skill_damage = ninja_moves[random_row+3][2]
+				attack_name = ninja_moves[random_row+3][0]
+			else:
+				attack_damage = ninja_moves[random_row][1]
+				skill_damage = ninja_moves[random_row][2]
+				attack_name = ninja_moves[random_row][0]
+		if enemy_skill - skill_damage >= 0:
+			attack_made = true
+	Globals.player_health -= attack_damage
+	enemy_skill -= skill_damage
+	ninja.play("attack spin")
+	await get_tree().create_timer(0.5).timeout
+	ninja.play("idle front")
+	Transition_screen.flash_transition()
+	await get_tree().create_timer(1).timeout
+	text_scroll_label.text = "ninja dealt a "+str(attack_name)+"!"
+	ninja_icon.visible = true
+	typewriter(text_scroll_label)
+	await get_tree().create_timer(1).timeout
 
 # copied functiosn from tutorial scene -> shld prolly make these global ://
 func fade_in(item, time: float = 1.0):
@@ -105,13 +175,17 @@ func ninja_fight():
 	fade_in(ninja)
 	fade_in(player)
 	player.play("idle front")
-	text_scroll.visible = true
-	text_scroll_label.visible = true
-	player_icon.visible = true
-	icon_border.visible = true
-	typewriter(text_scroll_label)
-	fade_in(moves_scroll)
-	show_move_options()
+	while enemy_health > 0:
+		if moves_made%2 == 0:
+			text_scroll.visible = true
+			text_scroll_label.visible = true
+			player_icon.visible = true
+			icon_border.visible = true
+			typewriter(text_scroll_label)
+			fade_in(moves_scroll)
+			show_move_options()
+		else:
+			ninja_attack()
 
 func _on_attack_button_pressed() -> void:
 	#hide_move_options(0.5) # cant use this bcs it covers the other buttons
@@ -130,6 +204,81 @@ func _on_back_button_pressed() -> void:
 # attack moves
 
 func _on_uppercut_button_pressed() -> void:
+	if Globals.player_skill >= 1:
+		fade_out(moves_scroll, 0.5)
+		hide_move_options(0.5)
+		hide_attack_options(0.5)
+		fade_out(back_button, 0.5)
+		player.play("attack")
+		await get_tree().create_timer(0.5).timeout
+		player.play("idle front")
+		Transition_screen.flash_transition()
+		await get_tree().create_timer(1).timeout
+		text_scroll_label.text = Globals.player_name+" dealt an uppercut!"
+		player_icon.visible = true
+		typewriter(text_scroll_label)
+		await get_tree().create_timer(1).timeout
+		enemy_health -= 10
+		Globals.player_skill -= 1
+		moves_made += 1
+	else:
+		text_scroll_label.add_theme_font_size_override("normal_font_size", 16)
+		text_scroll_label.text = "not enough skill to peform this move."
+		player_icon.visible = true
+		typewriter(text_scroll_label)
+	
+	
+	
+
+func _on_bck_kick_button_pressed() -> void:
+	if Globals.player_skill >= 2:
+		fade_out(moves_scroll, 0.5)
+		hide_move_options(0.5)
+		hide_attack_options(0.5)
+		fade_out(back_button, 0.5)
+		player.play("attack")
+		await get_tree().create_timer(0.5).timeout
+		player.play("idle front")
+		Transition_screen.flash_transition()
+		await get_tree().create_timer(1).timeout
+		text_scroll_label.text = Globals.player_name+" dealt a backwards kick!"
+		player_icon.visible = true
+		typewriter(text_scroll_label)
+		await get_tree().create_timer(1).timeout
+		enemy_health -= 15
+		Globals.player_skill -= 2
+		moves_made += 1
+	else:
+		text_scroll_label.add_theme_font_size_override("normal_font_size", 16)
+		text_scroll_label.text = "not enough skill to peform this move."
+		player_icon.visible = true
+		typewriter(text_scroll_label)
+
+func _on_dash_punch_button_pressed() -> void:
+	if Globals.player_skill >= 4:
+		fade_out(moves_scroll, 0.5)
+		hide_move_options(0.5)
+		hide_attack_options(0.5)
+		fade_out(back_button, 0.5)
+		player.play("attack")
+		await get_tree().create_timer(0.5).timeout
+		player.play("idle front")
+		Transition_screen.flash_transition()
+		await get_tree().create_timer(1).timeout
+		text_scroll_label.text = Globals.player_name+" dealt a dash punch!"
+		player_icon.visible = true
+		typewriter(text_scroll_label)
+		await get_tree().create_timer(1).timeout
+		enemy_health -= 20
+		Globals.player_skill -= 4
+		moves_made += 1
+	else:
+		text_scroll_label.add_theme_font_size_override("normal_font_size", 16)
+		text_scroll_label.text = "not enough skill to peform this move."
+		player_icon.visible = true
+		typewriter(text_scroll_label)
+
+func _on_lgh_kick_button_pressed() -> void:
 	fade_out(moves_scroll, 0.5)
 	hide_move_options(0.5)
 	hide_attack_options(0.5)
@@ -139,23 +288,9 @@ func _on_uppercut_button_pressed() -> void:
 	player.play("idle front")
 	Transition_screen.flash_transition()
 	await get_tree().create_timer(1).timeout
-	text_scroll_label.text = Globals.player_name+" dealt an uppercut!"
+	text_scroll_label.text = Globals.player_name+" dealt a light kick!"
 	player_icon.visible = true
 	typewriter(text_scroll_label)
 	await get_tree().create_timer(1).timeout
-	enemy_health -= 10
-	Globals.player_skill -= 1
-	
-	
-	
-
-func _on_bck_kick_button_pressed() -> void:
-	enemy_health -= 15
-	Globals.player_skill -= 2
-
-func _on_dash_punch_button_pressed() -> void:
-	enemy_health -= 20
-	Globals.player_skill -= 4
-
-func _on_lgh_kick_button_pressed() -> void:
 	enemy_health -= 5
+	moves_made += 1
